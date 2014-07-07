16ad3f941c9246fa07f665445047155b51e76405
bubbleknight <sungchou@ip-10-195-104-178.wireless-wmc.nor.ou.edu>
2014-07-07 16:15:20 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Sung Chou on 7/7/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
