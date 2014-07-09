ffa44a684b4f6824ae1e6c5d086ba53a365b8d41
bubbleknight <sungchou@ip-10-195-104-178.wireless-wmc.nor.ou.edu>
2014-07-09 09:00:06 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -43,6 +43,22 @@
     CGPoint launchDirection = ccp(1, 0);
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
+    
+    // ensure followed object is in visible are when startgin
+    self.position = ccp(0, 0);
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
+    [self runAction:follow];
+    
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
