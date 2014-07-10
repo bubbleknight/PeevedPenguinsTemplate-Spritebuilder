dda735cf4c8b61c58d8a8bc786d7d559a05e6e85
bubbleknight <sungchou@ip-10-195-104-178.wireless-wmc.nor.ou.edu>
2014-07-10 16:09:08 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Sung Chou on 7/10/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
